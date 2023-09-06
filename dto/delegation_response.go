9cf96054e245ea248573a27bfc96644c119bd27f
pavva91 <valerio.mattioli@protonmail.com>
2023-09-06 11:18:15 +0200

    Add functions to translate Model into Response DTO

--- dto/delegation_response.go
+++ dto/delegation_response.go
@@ -1,10 +1,29 @@
 package dto
 
-import "time"
+import (
+	"time"
+
+	"github.com/pavva91/tezos-delegation-service/models"
+)
 
 type DelegationResponse struct {
 	Timestamp time.Time `json:"timestamp"`
 	Amount    string    `json:"amount"`
 	Delegator string    `json:"delegator"`
 	Block     string    `json:"block"`
+}
+
+func (dto *DelegationResponse) ToDto(delegation models.Delegation) {
+	dto.Timestamp = delegation.Timestamp
+	dto.Amount = delegation.Amount
+	dto.Delegator = delegation.Delegator
+	dto.Block = delegation.Block
+}
+
+func (dto DelegationResponse) ToDtos(delegationModels []models.Delegation) (delegationDtos []DelegationResponse) {
+	delegationDtos = make([]DelegationResponse, len(delegationModels))
+	for i, delegationModel := range delegationModels {
+		delegationDtos[i].ToDto(delegationModel)
+	}
+	return delegationDtos
 }
