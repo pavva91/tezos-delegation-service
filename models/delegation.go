7c13c394d709fc089a8739e6424cdfb5430fc2b5
pavva91 <valerio.mattioli@protonmail.com>
2023-09-06 11:18:43 +0200

    Stick to block as string

--- models/delegation.go
+++ models/delegation.go
@@ -8,11 +8,11 @@ import (
 
 type Delegation struct {
 	gorm.Model `swaggerignore:"true"`
-	// TODO: Timestamp must be ISO-8601 
+	// TODO: Timestamp must be ISO-8601
 	// TODO: time.RFC3339 <-> time.Now().UTC()
 
-	Timestamp  time.Time `json:"timestamp"`
-	Amount     string    `json:"amount" binding:"required"`
-	Delegator  string    `json:"delegator" binding:"required"`
-	Block      int       `json:"block" binding:"required"`
+	Timestamp time.Time `json:"timestamp"`
+	Amount    string    `json:"amount" binding:"required"`
+	Delegator string    `json:"delegator" binding:"required"`
+	Block     string    `json:"block" binding:"required"`
 }
