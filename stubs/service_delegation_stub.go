c536a0649108c8cda59150f28e2b980f91e98b14
pavva91 <valerio.mattioli@protonmail.com>
2023-09-07 11:49:47 +0200

    Add Stub Service Layer

--- stubs/service_delegation_stub.go
+++ stubs/service_delegation_stub.go
@@ -4,8 +4,13 @@ import "github.com/pavva91/tezos-delegation-service/models"
 
 type DelegationServiceStub struct {
 	ListDelegationsFn func() ([]models.Delegation, error)
+	PollDelegationsFn func(int) error
 }
 
-func (stub DelegationServiceStub) ListAllDelegations() ([]models.Delegation, error) {
+func (stub DelegationServiceStub) ListDelegations() ([]models.Delegation, error) {
 	return stub.ListDelegationsFn()
+}
+
+func (stub DelegationServiceStub) PollDelegations(periodInSeconds int) error {
+	return stub.PollDelegationsFn(periodInSeconds)
 }
