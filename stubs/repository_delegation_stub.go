e027ef04d0f3b0dbadf4a6a2a8b6bd8e1afe9389
pavva91 <valerio.mattioli@protonmail.com>
2023-09-07 11:49:24 +0200

    Add Stub Repository Layer

--- stubs/repository_delegation_stub.go
+++ stubs/repository_delegation_stub.go
@@ -3,9 +3,14 @@ package stubs
 import "github.com/pavva91/tezos-delegation-service/models"
 
 type DelegationRepositoryStub struct {
-	ListDelegationsFn func() ([]models.Delegation, error)
+	ListFn func() ([]models.Delegation, error)
+	CreateFn func(*models.Delegation) (*models.Delegation, error)
 }
 
-func (stub DelegationRepositoryStub) ListDelegations() ([]models.Delegation, error) {
-	return stub.ListDelegationsFn()
+func (stub DelegationRepositoryStub) List() ([]models.Delegation, error) {
+	return stub.ListFn()
+}
+
+func (stub DelegationRepositoryStub) Create(delegation *models.Delegation) (*models.Delegation, error) {
+	return stub.CreateFn(delegation)
 }
